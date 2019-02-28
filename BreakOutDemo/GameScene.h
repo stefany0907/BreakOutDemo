acb71930d0fa162209c3ada0936718e2179188d7
Matt Chen <mattchen@Matts-MacBook-Air.local>
2019-02-27 20:21:05 -0800

    Initial Commit

---
+++ BreakOutDemo/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  BreakOutDemo
+//
+//  Created by Matt Chen on 2/27/19.
+//  Copyright © 2019 Matt Chen. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
